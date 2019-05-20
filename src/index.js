3f03caf109ca92298e96ded04cc8adc18c2398b0
atoscano <atoscano@correo.unicordoba.edu.co>
2019-05-20 14:07:38 -0500

    Leccion 2: conexion a una base de datos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-const express = require("express");
-const app = express();
+const routes = require("./bin/routes");
+const { app } = require("./bin/routes");
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
-
-app.get("/users", (req, res) => {
-  let users = [
-    { name: "Jhon", lastName: "Doe", age: 30 },
-    { name: "María", lastName: "Sharápova", age: 32 }
-  ];
-  res.send(users);
-});
 app.listen(3000, () => {
   console.log("Server on port http://localhost:3000");
 });
