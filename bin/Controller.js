958c0a35249e86f8d237c0699a4e06580da07fae
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2019-07-26 22:30:07 +0000

    Routes

--- bin/Controller.js
+++ bin/Controller.js
@@ -5,181 +5,182 @@ const Song = require("./models/Song");
 const Artist = require("./models/Artist");
 
 class Controller {
-  constructor() {
-    //al crearse el objeto se establece la conexion
-    this.connect();
-  }
-
-  async connect() {
-    try {
-      //se intenta establecer una conexion con los datos de conexion
-      await mongoose.connect(
-        "mongodb://localhost:27017/my_music_collection_db",
-        { useNewUrlParser: true }
-      );
-      //en caso de esablecer la conexion, se muestra en consola este mensaje
-      console.log("Connected databases.");
-    } catch (e) {
-      //en caso de haber un error se muestra en consola el error
-      console.error(e);
+    constructor() {
+        //al crearse el objeto se establece la conexion
+        this.connect();
     }
-  }
-  //CRUD https://coursework.vschool.io/mongoose-crud/
-  //CRUD https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
 
-  //CRUD user
-  setUser(user, res) {
-    // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
-    User.create(user, function(err, newUser) {
-      // sihay error se reporta
-      if (err) throw err;
-      // se retorna la informacion con el nuevo usuario creado
-      res.send({ status: 200, nU: newUser });
-    });
-  }
-
-  //metodo para buscar todos los usuarios
-  getUsers(req, res) {
-    //en el modelo User se ejecuta el find sin ninguna condicion...
-    User.find({}, (err, users) => {
-      //en caso de haberse presentado un error se ejecuta el error
-      if (err) throw err;
-      //de lo contrario se retorna un objeto con todos los resultados
-      res.send({ allUsers: users });
-    });
-  }
-
-  getUser(id, res) {
-    //en el modelo User se ejecuta el find sin ninguna condicion...
-    User.find({ _id: id }, (err, user) => {
-      //en caso de haberse presentado un error se ejecuta el error
-      if (err) throw err;
-      //de lo contrario se retorna un objeto con todos los resultados
-      res.send({ User: user });
-    });
-  }
-
-  updateUser(user, res) {
-    //optenemos los campos que queremos actualizar
-    let { id, picture, password } = user;
-    //actualizamos teniendo en cuenta una condicion con el operador $set
-    //https://docs.mongodb.com/manual/reference/operator/update/set/
-    User.updateOne(
-      { _id: id },
-      { $set: { picture: picture, password: password } }
-    )
-      .then(rawResponse => {
-        res.send({ message: "User updated", raw: rawResponse });
-      })
-      .catch(err => {
-        if (err) throw err;
-      });
-  }
-
-  deleteUser(id, res) {
-    User.deleteOne({ _id: id }, function(err) {
-      if (err) throw err;
-      res.send({ message: "User has been deleted" });
-    });
-  } // remove, findByIdAndRemove, findOneAndRemove
-
-  //CRUD list
-  setUserList(user_id, list, res) {
-    list.user_id = user_id;
-    List.create(list, function(err, newList) {
-      if (err) return handleError(err);
-      res.send({ status: 200, id_list: newList._id });
-    });
-  }
-
-  getUserLists(id, res) {
-    List.find({ user_id: id }, function(err, userList) {
-      if (err) throw err;
-      res.send({ status: 200, user_list: userList });
-    });
-  }
-
-  getUserList(user_id, list_id, res) {
-    List.find({ _id: list_id, user_id: user_id })
-      .populate({
-        path: "song_id",
-        select: "name artists duraction",
-        populate: {
-          path: "artists",
-          select: "name1 name2 last_name1 last_name2"
+    async connect() {
+        try {
+            //se intenta establecer una conexion con los datos de conexion
+            await mongoose.connect(
+                "mongodb+srv://kikret:17DYcr6Zow5TYCSu@cluster0-9ek42.mongodb.net/test?retryWrites=true&w=majority",
+                //"mongodb://localhost:27017/my_music_collection_db",
+                { useNewUrlParser: true }
+            );
+            //en caso de esablecer la conexion, se muestra en consola este mensaje
+            console.log("Connected databases.");
+        } catch (e) {
+            //en caso de haber un error se muestra en consola el error
+            console.error(e);
         }
-      })
-      .exec(function(err, userListSong) {
-        if (err) throw err;
-        res.send({ status: 200, user_list_song: userListSong });
-      });
-  }
+    }
+    //CRUD https://coursework.vschool.io/mongoose-crud/
+    //CRUD https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/
 
-  //CRUD song
-  setSong(song, res) {
-    Song.create(song, function(err, newSong) {
-      if (err) throw err;
-      res.send({ status: 200, id_song: newSong._id });
-    });
-  }
-  getSongs(res) {
-    Song.find().exec(function(err, songs) {
-      if (err) throw err;
-      res.send({ status: 200, artists: songs });
-    });
-  }
-  getSong(id, res) {
-    Song.find({ _id: id }).exec(function(err, song) {
-      if (err) throw err;
-      res.send({ status: 200, artists: song });
-    });
-  }
+    //CRUD user
+    setUser(user, res) {
+        // Se recibe el nuevo usuario en la variable user y se crea a partir del modelo
+        User.create(user, function(err, newUser) {
+            // sihay error se reporta
+            if (err) throw err;
+            // se retorna la informacion con el nuevo usuario creado
+            res.send({ status: 200, nU: newUser });
+        });
+    }
 
-  updateList(song_id, id_user, id_list, res) {
-    //actualizamos teniendo en cuenta una condicion con el operador $push
-    // https://docs.mongodb.com/manual/reference/operator/update/push/
-    List.updateOne({ _id: id_list }, { $push: { song_id: song_id } })
-      .then(rawResponse => {
-        res.send({ message: "List updated", raw: rawResponse });
-      })
-      .catch(err => {
-        if (err) throw err;
-      });
-  }
+    //metodo para buscar todos los usuarios
+    getUsers(req, res) {
+        //en el modelo User se ejecuta el find sin ninguna condicion...
+        User.find({}, (err, users) => {
+            //en caso de haberse presentado un error se ejecuta el error
+            if (err) throw err;
+            //de lo contrario se retorna un objeto con todos los resultados
+            res.send({ allUsers: users });
+        });
+    }
 
-  //CRUD artist
-  setArtist(artist, res) {
-    Artist.create(artist, function(err, newArtist) {
-      if (err) throw err;
-      res.send({ status: 200, id_artist: newArtist._id });
-    });
-  }
+    getUser(id, res) {
+        //en el modelo User se ejecuta el find sin ninguna condicion...
+        User.find({ _id: id }, (err, user) => {
+            //en caso de haberse presentado un error se ejecuta el error
+            if (err) throw err;
+            //de lo contrario se retorna un objeto con todos los resultados
+            res.send({ User: user });
+        });
+    }
 
-  getArtists(res) {
-    Artist.find().exec(function(err, artist) {
-      if (err) throw err;
-      res.send({ status: 200, artists: artist });
-    });
-  }
+    updateUser(user, res) {
+        //optenemos los campos que queremos actualizar
+        let { id, picture, password } = user;
+        //actualizamos teniendo en cuenta una condicion con el operador $set
+        //https://docs.mongodb.com/manual/reference/operator/update/set/
+        User.updateOne(
+            { _id: id },
+            { $set: { picture: picture, password: password } }
+        )
+            .then(rawResponse => {
+                res.send({ message: "User updated", raw: rawResponse });
+            })
+            .catch(err => {
+                if (err) throw err;
+            });
+    }
 
-  getArtist(id, res) {
-    Artist.find({ _id: id }).exec(function(err, artist) {
-      if (err) throw err;
-      res.send({ status: 200, artists: artist });
-    });
-  }
+    deleteUser(id, res) {
+        User.deleteOne({ _id: id }, function(err) {
+            if (err) throw err;
+            res.send({ message: "User has been deleted" });
+        });
+    } // remove, findByIdAndRemove, findOneAndRemove
 
-  updateSong(song_id, artists, res) {
-    //actualizamos teniendo en cuenta una condicion con el operador $push
-    // https://docs.mongodb.com/manual/reference/operator/update/push/
-    Song.updateOne({ _id: song_id }, { $push: { artists: artists } })
-      .then(rawResponse => {
-        res.send({ message: "Song updated", raw: rawResponse });
-      })
-      .catch(err => {
-        if (err) throw err;
-      });
-  }
+    //CRUD list
+    setUserList(user_id, list, res) {
+        list.user_id = user_id;
+        List.create(list, function(err, newList) {
+            if (err) return handleError(err);
+            res.send({ status: 200, id_list: newList._id });
+        });
+    }
+
+    getUserLists(id, res) {
+        List.find({ user_id: id }, function(err, userList) {
+            if (err) throw err;
+            res.send({ status: 200, user_list: userList });
+        });
+    }
+
+    getUserList(user_id, list_id, res) {
+        List.find({ _id: list_id, user_id: user_id })
+            .populate({
+                path: "song_id",
+                select: "name artists duraction",
+                populate: {
+                    path: "artists",
+                    select: "name1 name2 last_name1 last_name2"
+                }
+            })
+            .exec(function(err, userListSong) {
+                if (err) throw err;
+                res.send({ status: 200, user_list_song: userListSong });
+            });
+    }
+
+    //CRUD song
+    setSong(song, res) {
+        Song.create(song, function(err, newSong) {
+            if (err) throw err;
+            res.send({ status: 200, id_song: newSong._id });
+        });
+    }
+    getSongs(res) {
+        Song.find().exec(function(err, songs) {
+            if (err) throw err;
+            res.send({ status: 200, artists: songs });
+        });
+    }
+    getSong(id, res) {
+        Song.find({ _id: id }).exec(function(err, song) {
+            if (err) throw err;
+            res.send({ status: 200, artists: song });
+        });
+    }
+
+    updateList(song_id, id_user, id_list, res) {
+        //actualizamos teniendo en cuenta una condicion con el operador $push
+        // https://docs.mongodb.com/manual/reference/operator/update/push/
+        List.updateOne({ _id: id_list }, { $push: { song_id: song_id } })
+            .then(rawResponse => {
+                res.send({ message: "List updated", raw: rawResponse });
+            })
+            .catch(err => {
+                if (err) throw err;
+            });
+    }
+
+    //CRUD artist
+    setArtist(artist, res) {
+        Artist.create(artist, function(err, newArtist) {
+            if (err) throw err;
+            res.send({ status: 200, id_artist: newArtist._id });
+        });
+    }
+
+    getArtists(res) {
+        Artist.find().exec(function(err, artist) {
+            if (err) throw err;
+            res.send({ status: 200, artists: artist });
+        });
+    }
+
+    getArtist(id, res) {
+        Artist.find({ _id: id }).exec(function(err, artist) {
+            if (err) throw err;
+            res.send({ status: 200, artists: artist });
+        });
+    }
+
+    updateSong(song_id, artists, res) {
+        //actualizamos teniendo en cuenta una condicion con el operador $push
+        // https://docs.mongodb.com/manual/reference/operator/update/push/
+        Song.updateOne({ _id: song_id }, { $push: { artists: artists } })
+            .then(rawResponse => {
+                res.send({ message: "Song updated", raw: rawResponse });
+            })
+            .catch(err => {
+                if (err) throw err;
+            });
+    }
 }
 // se exporta elobjeto controlador crado a partir de la clase Controller
 exports.controller = new Controller();
--- bin/routes.js
+++ bin/routes.js
@@ -4,116 +4,131 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+//midware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 //llamamos el controlador que se encarga de gestionar la base de datos
-const { controller } = require("./Controller");
+const { controller } =
+    require("./Controller");
 
 //creamos la ruta raiz para enviar un mensaje de bienvenida con la version
 app.get("/version", (req, res) => {
-  //retornamos un mensaje
-  res.send("My Music v1");
+    //retornamos un mensaje
+    res.send({ version: "1.0.0" });
 });
 
 //Routes for user
 //Agregar a un usuario
 app.post("/users", function(req, res) {
-  let { user } = req.body;
-  controller.setUser(user, res);
+    let { user } = req.body;
+    controller.setUser(user, res);
 });
 
 //creamos la ruta users que deberá traer todos los usuarios
 app.get("/users", (req, res) => {
-  //llamamos el metodo getUser del objeto controller, este se encarga de buscar todos los usuarios
-  //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
-  controller.getUsers(req, res);
+    //llamamos el metodo getUser del objeto controller, este se encarga de buscar todos los usuarios
+    //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
+    controller.getUsers(req, res);
 });
 
 //Traer a un usuario por su id
-http: app.get("/users/:id", function(req, res) {
-  let { id } = req.params;
-  controller.getUser(id, res);
+app.get("/users/:id", function(req, res) {
+    let { id } = req.params;
+    controller.getUser(id, res);
 });
 
 //Actualizar a un usuario por su id
 app.put("/users/:id", function(req, res) {
-  let user = req.body.user;
-  user.id = req.params.id;
-  controller.updateUser(user, res);
+    let user = req.body.user;
+    user.id = req.params.id;
+    controller.updateUser(user, res);
 });
 
 //Eliminar a un usuario por su id
 app.delete("/users/:id", function(req, res) {
-  let { id } = req.params;
-  controller.deleteUser(id, res);
+    let { id } = req.params;
+    controller.deleteUser(id, res);
 });
 
 //Routes for list
 //Agregar una lista de usuario
 app.post("/users/:user_id/lists", (req, res) => {
-  let { user_id } = req.params;
-  let { list } = req.body;
-  controller.setUserList(user_id, list, res);
+    let { user_id } = req.params;
+    let { list } = req.body;
+    controller.setUserList(user_id, list, res);
 });
 
 //Traer las listas de un usuario
 app.get("/users/:id/lists", function(req, res) {
-  let { id } = req.params;
-  controller.getUserLists(id, res);
+    let { id } = req.params;
+    controller.getUserLists(id, res);
 });
 
 //Traer una lista específica de un usuario
 app.get("/users/:user_id/lists/:list_id", (req, res) => {
-  let { user_id, list_id } = req.params;
-  controller.getUserList(user_id, list_id, res);
+    let { user_id, list_id } = req.params;
+    controller.getUserList(user_id, list_id, res);
 });
 
 //Routes for song
 //Guardar una cancion
 app.post("/songs", (req, res) => {
-  let { song } = req.body;
-  controller.setSong(song, res);
+    let { song } = req.body;
+    controller.setSong(song, res);
 });
 
 //traer todas las canciones
 app.get("/songs", (req, res) => {
-  controller.getSongs(res);
+    controller.getSongs(res);
 });
 
 //traer una cancion
 app.get("/songs/:id", (req, res) => {
-  let { id } = req.params;
-  controller.getSong(id, res);
+    let { id } = req.params;
+    controller.getSong(id, res);
 });
 
 //Actualizar una lista con canciones
 app.put("/users/:id_user/lists/:id_list/", function(req, res) {
-  let { song_id } = req.body;
-  let { id_user, id_list } = req.params;
-  controller.updateList(song_id, id_user, id_list, res);
+    let { song_id } = req.body;
+    let { id_user, id_list } = req.params;
+    controller.updateList(song_id, id_user, id_list, res);
 });
 
 //Routes for Artista
 //Agregar un artista
 app.post("/artists/", (req, res) => {
-  let { artist } = req.body;
-  controller.setArtist(artist, res);
+    let { artist } = req.body;
+    controller.setArtist(artist, res);
 });
 
 // Traer todos los artistas
 app.get("/artists/", (req, res) => {
-  controller.getArtists(res);
+    controller.getArtists(res);
 });
 
 //traer un artista por su id
 app.get("/artists/:id", (req, res) => {
-  let { id } = req.params;
-  controller.getArtist(id, res);
+    let { id } = req.params;
+    controller.getArtist(id, res);
 });
 
 //Actualizar canciones con artistas
 app.put("/songs/:id_song/", function(req, res) {
-  let { id_song } = req.params;
-  let { artists } = req.body;
-  controller.updateSong(id_song, artists, res);
+    let { id_song } = req.params;
+    let { artists } = req.body;
+    controller.updateSong(id_song, artists, res);
 });
 //exportamos la constante app con toda la configuracion de las rutas
 exports.app = app;
